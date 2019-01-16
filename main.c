c6b4aecb2e49bc0e6a12cf475d010084cc9bffec
Zhang Miaolei <zmlcc@outlook.com>
2019-01-16 18:40:12 +0800

    tmp 190116

--- main.c
+++ main.c
@@ -6,72 +6,64 @@
 #include <sys/queue.h>
 
 #define RTE_DIM(a) (sizeof(a) / sizeof((a)[0]))
-#define USE_LEN_MAX 32
-#define HELP_ELN_MAX 1024
+#define NAME_LEN_MAX 32
+#define HELP_LEN_MAX 1024
 
 typedef void (*command_handle_t)(char **tokens,
                                  uint32_t n_tokens);
 
-
+TAILQ_HEAD(command_list, command);
 
 struct command
 {
-    TAILQ_ENTRY(command)
-    node;
-    char use[USE_LEN_MAX];
+    TAILQ_ENTRY(command) node;
+    char *name;
     char *help;
     command_handle_t handle;
     struct command *parent;
-    struct command_list *subcommand;
+    struct command_list* subcommand;
 };
 
-TAILQ_HEAD(command_list, command);
-
-int command_init(struct command *cmd)
+static inline int command_init(struct command *cmd)
 {
     cmd->parent = NULL;
+    cmd->subcommand = malloc(sizeof(struct command_list));
     TAILQ_INIT(cmd->subcommand);
     return 0;
 }
 
-struct command *command_new(const char *use, const char *help)
+struct command *command_new(const char *name, const char *help)
 {
     struct command *cmd;
 
-    if ((use == NULL) || (help == NULL))
+    if ((name == NULL) || (help == NULL))
     {
         return NULL;
     }
 
-    printf("E1");
     cmd = calloc(1, sizeof(struct command));
     if (cmd == NULL)
     {
         return NULL;
     }
 
-    strncpy(cmd->use, use, sizeof(use));
+    cmd->name = calloc(1, NAME_LEN_MAX + 1);
+    strncpy(cmd->name, name, NAME_LEN_MAX);
 
-    cmd->help = calloc(1, HELP_ELN_MAX + 1);
-    strncpy(cmd->help, help, HELP_ELN_MAX);
+    cmd->help = calloc(1, HELP_LEN_MAX + 1);
+    strncpy(cmd->help, help, HELP_LEN_MAX);
 
-    cmd->parent = NULL;
-    // cmd->subcommand = &TAILQ_HEAD_INITIALIZER(command_list);
-    printf("E1");
-
-    TAILQ_INIT(cmd->subcommand);
-    printf("E1");
+    command_init(cmd);
 
     return cmd;
-    return NULL;
 }
 
-struct command *command_list_find(struct command_list *list, const char *use)
+struct command *command_list_find(struct command_list* list, const char *name)
 {
     struct command *cmd;
     TAILQ_FOREACH(cmd, list, node)
     {
-        if (strcmp(cmd->use, use) == 0)
+        if (strcmp(cmd->name, name) == 0)
         {
             return cmd;
         }
@@ -113,44 +105,48 @@ void command_exec(struct command *cmd, char **tokens, u_int32_t n_tokens)
         cmd->handle(tokens, n_tokens);
         return;
     }
-    if (cmd->subcommand != NULL)
-    {
-        // show subcommnad
-        return;
-    }
+    if TAILQ_EMPTY(cmd->subcommand)
+        {
+            // show subcommnad
+            return;
+        }
     // show error
 }
 
+void command_nprintf();
 void command_show_subcommand(struct command *cmd, char *out,
                              size_t out_size)
 {
-    if (cmd->subcommand == NULL)
-    {
-        snprintf(out, out_size, "");
-        return;
-    }
+    if TAILQ_EMPTY(cmd->subcommand)
+        {
+            snprintf(out, out_size, "");
+            return;
+        }
 
-    size_t max_use_len;
+    size_t max_use_len = 0;
     struct command *subcmd;
-    TAILQ_FOREACH(subcmd, cmd->subcommand, node){
-        if (strlen(subcmd->use) > max_use_len){
-            max_use_len = strlen(subcmd->use);
+    TAILQ_FOREACH(subcmd, cmd->subcommand, node)
+    {
+        if (strlen(subcmd->name) > max_use_len)
+        {
+            max_use_len = strlen(subcmd->name);
         }
     }
 
-    char expr[16];
-    snprintf(expr, 16, "%%-%ds\t%%s\n", max_use_len);
+    char expr[32];
+    snprintf(expr, 32, "%%-%ds\t%%s\n", max_use_len);
 
     char *target = out;
-    TAILQ_FOREACH(subcmd, cmd->subcommand, node){
-        size_t unused_size = out_size - strlen(out);
-        if (0 == unused_size) {
+    TAILQ_FOREACH(subcmd, cmd->subcommand, node)
+    {
+        size_t unused_size = out + out_size - target;
+        int used_size = snprintf(target, unused_size, expr, subcmd->name, subcmd->help);
+        if (used_size < 0 || used_size >= unused_size) {
             break;
         }
-        target += snprintf(target, unused_size, expr, cmd->use, cmd->help);
+        target += used_size;
     }
 }
-
 
 int main(int argc, char **argv)
 {
@@ -171,12 +167,17 @@ int main(int argc, char **argv)
     // printf("%s\n", argv[3]);
     // printf("\n%d\n", sizeof(argv[3][0]));
 
-    struct command* root_cmd=command_new("root", "root cmd");
-    // struct command* cmd1=command_new("cmd1", "this is cmd 1");
-    // struct command* cmd2=command_new("cmd2", "this is cmd 2");
-    // command_add_subcommand(root_cmd, cmd1);
-    // command_add_subcommand(root_cmd, cmd2);
+    struct command *root_cmd = command_new("root", "this is root cmd");
+    struct command *cmd1 = command_new("ffdsa", "fdsa");
+    struct command *cmd2 = command_new("cmd2verylong", "this is cmd 2");
+    char output[100];
+    command_add_subcommand(root_cmd, cmd1);
+    printf("\n");
+    command_show_subcommand(root_cmd, output, 10);
+    printf("%s\n", output);
 
-    // char output[1000];
-    // command_show_subcommand(root_cmd, output, 1000);
+    command_add_subcommand(root_cmd, cmd2);
+    printf("\n");
+    command_show_subcommand(root_cmd, output, 20);
+    printf("%s\n", output);
 }
